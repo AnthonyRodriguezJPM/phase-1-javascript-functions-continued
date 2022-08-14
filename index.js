a96f40dc677dad7de7945e250e437f2f5e4685af
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-13 21:59:41 -0500

    commit tree 4d3ac2ed7e1203d79131987592ecb4fbb34709e1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+
+function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(star='*') {
+    const innerFunction=function (lines='special') {
+        return console.log(`You are ${star}a ${lines}${star}!`);
+    };
+    return innerFunction;
+}
+const storedFunction=wrapAdjective('*');
+
+wrapAdjective('%')('dedicated programmer');
+
+
